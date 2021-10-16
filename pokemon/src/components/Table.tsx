7c6500ee74a29139e581b5de79525faa58f9b197
Monika <monika-luu@hotmail.com>
2021-10-16 11:02:52 +0200

    #1 changed table

--- pokemon/src/components/Table.tsx
+++ pokemon/src/components/Table.tsx
@@ -1,33 +1,41 @@
 import React from 'react';
-import MUIDataTable from "mui-datatables";
+import { DataGrid, GridColDef } from '@mui/x-data-grid';
 
 const Table = () => {
-    const columns = ["Name", "Picture", "ID", "Weight", "Height", "Types"];
 
-    const data = [
-        ["Joe James", "Test Corp", "Yonkers", "NY", "Dummy", "Values"],
-        ["John Walsh", "Test Corp", "Hartford", "CT"],
-        ["Bob Herm", "Test Corp", "Tampa", "FL"],
-        ["James Houston", "Test Corp", "Dallas", "TX"],
+    const columns: GridColDef[] = [
+        { field: 'id', headerName: 'ID', width: 100 },
+        { field: 'name', headerName: 'Name', width: 130 },
+        { field: 'picture', headerName: 'Picture', width: 130 },
+        { field: 'height', headerName: 'Height', width: 130 },
+        { field: 'weight', headerName: 'Weight', width: 130 },
+        { field: 'type', headerName: 'Type', width: 130 },
     ];
 
-    const options = {
-        selectableRowsHeader: false,
-        displayRowCheckbox: false
-    }
-
-
+    const rows = [
+        { id: 1, name: 'Snow', picture: 'Jon', height: 35, weight: 30, type: 'Green' },
+        { id: 2, name: 'Snow', picture: 'Jon', height: 35, weight: 30, type: 'Green' },
+        { id: 3, name: 'Snow', picture: 'Jon', height: 35, weight: 30, type: 'Green' },
+        { id: 4, name: 'Snow', picture: 'Jon', height: 35, weight: 30, type: 'Green' },
+        { id: 5, name: 'Snow', picture: 'Jon', height: 35, weight: 30, type: 'Green' },
+        { id: 6, name: 'Snow', picture: 'Jon', height: 35, weight: 30, type: 'Green' },
+        { id: 7, name: 'Snow', picture: 'Jon', height: 35, weight: 30, type: 'Green' },
+        { id: 8, name: 'Snow', picture: 'Jon', height: 35, weight: 30, type: 'Green' },
+        { id: 9, name: 'Snow', picture: 'Jon', height: 35, weight: 30, type: 'Green' },
+        { id: 10, name: 'Snow', picture: 'Jon', height: 35, weight: 30, type: 'Green' }
+    ];
 
     return (
-        <MUIDataTable
-            title={"Pokemons"}
-            data={data}
-            columns={columns}
-            options={options}
-        />
-    )
+        <div style={{ height: 700, width: '100%' }}>
+            <DataGrid
+                rows={rows}
+                columns={columns}
+                pageSize={10}
+                rowsPerPageOptions={[1]}
+            />
+        </div>
+    );
 }
-
 
 export default Table;
 
