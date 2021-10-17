313a2f3f84dffe5bff184e310cdff7a1fc67aa80
Monika <monika-luu@hotmail.com>
2021-10-17 19:37:16 +0200

    disabled hide for some columns

--- pokemon/src/components/Table.tsx
+++ pokemon/src/components/Table.tsx
@@ -4,17 +4,17 @@ import { DataGrid, GridColDef } from '@mui/x-data-grid';
 const Table = () => {
 
     const columns: GridColDef[] = [
-        { field: 'id', headerName: 'ID', width: 100 },
-        { field: 'name', headerName: 'Name', width: 130 },
-        { field: 'picture', headerName: 'Picture', width: 130 },
+        { field: 'id', headerName: 'ID', width: 100, disableColumnMenu: true, sortable: true },
+        { field: 'name', headerName: 'Name', width: 130, disableColumnMenu: true, sortable: false },
+        { field: 'picture', headerName: 'Picture', width: 130, sortable: false },
         { field: 'height', headerName: 'Height', width: 130 },
         { field: 'weight', headerName: 'Weight', width: 130 },
-        { field: 'type', headerName: 'Type', width: 130 },
+        { field: 'type', headerName: 'Type', width: 130, sortable: false },
     ];
 
     const rows = [
         { id: 1, name: 'Snow', picture: 'Jon', height: 35, weight: 30, type: 'Green' },
-        { id: 2, name: 'Snow', picture: 'Jon', height: 35, weight: 30, type: 'Green' },
+        { id: 2, name: 'Betty', picture: 'Jon', height: 40, weight: 30, type: 'Green' },
         { id: 3, name: 'Snow', picture: 'Jon', height: 35, weight: 30, type: 'Green' },
         { id: 4, name: 'Snow', picture: 'Jon', height: 35, weight: 30, type: 'Green' },
         { id: 5, name: 'Snow', picture: 'Jon', height: 35, weight: 30, type: 'Green' },
@@ -22,8 +22,20 @@ const Table = () => {
         { id: 7, name: 'Snow', picture: 'Jon', height: 35, weight: 30, type: 'Green' },
         { id: 8, name: 'Snow', picture: 'Jon', height: 35, weight: 30, type: 'Green' },
         { id: 9, name: 'Snow', picture: 'Jon', height: 35, weight: 30, type: 'Green' },
-        { id: 10, name: 'Snow', picture: 'Jon', height: 35, weight: 30, type: 'Green' }
+        { id: 10, name: 'Candy', picture: 'Jon', height: 35, weight: 30, type: 'Green' }
     ];
+
+    //A function that hides a selected column
+    /*
+    function showColumn() {
+
+    }
+
+
+    function hideColumn() {
+
+    }*/
+
 
     return (
         <div style={{ height: 700, width: '100%' }}>
@@ -32,8 +44,10 @@ const Table = () => {
                 columns={columns}
                 pageSize={10}
                 rowsPerPageOptions={[1]}
+                disableSelectionOnClick
             />
-        </div>
+
+        </div >
     );
 }
 
